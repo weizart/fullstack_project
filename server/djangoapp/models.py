c1dd648e87010b7729df8adc208f5e0e7e61692f
weizart <13546450703@163.com>
2024-12-14 18:00:10 +0800

    add django app

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,46 @@
-# Uncomment the following imports before adding the Model code
-
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+# Car Make model
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name  # Return the name as the string representation
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+# Car Model model
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    name = models.CharField(max_length=100)
+    dealer_id = models.IntegerField()  # Reference to dealer in Cloudant
+    
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('SPORTS', 'Sports Car'),
+        ('PICKUP', 'Pickup Truck'),
+        ('VAN', 'Van'),
+        ('MINIVAN', 'Mini Van'),
+        ('CONVERTIBLE', 'Convertible'),
+    ]
+    type = models.CharField(max_length=15, choices=CAR_TYPES, default='SEDAN')
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    engine = models.CharField(max_length=100)
+    transmission = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year})"  # Return a descriptive string representation
--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,2 +1,42 @@
+from .models import CarMake, CarModel
+
 def initiate():
-    print("Populate not implemented. Add data manually")
+    car_make_data = [
+        {"name":"NISSAN", "description":"Great cars. Japanese technology"},
+        {"name":"Mercedes", "description":"Great cars. German technology"},
+        {"name":"Audi", "description":"Great cars. German technology"},
+        {"name":"Kia", "description":"Great cars. Korean technology"},
+        {"name":"Toyota", "description":"Great cars. Japanese technology"},
+    ]
+    car_make_instances = []
+    for data in car_make_data:
+            car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
+    # Create CarModel instances with the corresponding CarMake instances
+    car_model_data = [
+      {"name":"Pathfinder", "type":"SUV", "year": 2023, "car_make":car_make_instances[0], "dealer_id": 1, "price": 50000.00, "engine": "V6", "transmission": "Automatic"},
+      {"name":"Qashqai", "type":"SUV", "year": 2023, "car_make":car_make_instances[0], "dealer_id": 1, "price": 45000.00, "engine": "4-cylinder", "transmission": "CVT"},
+      {"name":"XTRAIL", "type":"SUV", "year": 2023, "car_make":car_make_instances[0], "dealer_id": 1, "price": 47000.00, "engine": "4-cylinder", "transmission": "CVT"},
+      {"name":"A-Class", "type":"SUV", "year": 2023, "car_make":car_make_instances[1], "dealer_id": 2, "price": 55000.00, "engine": "4-cylinder", "transmission": "Automatic"},
+      {"name":"C-Class", "type":"SUV", "year": 2023, "car_make":car_make_instances[1], "dealer_id": 2, "price": 65000.00, "engine": "4-cylinder", "transmission": "Automatic"},
+      {"name":"E-Class", "type":"SUV", "year": 2023, "car_make":car_make_instances[1], "dealer_id": 2, "price": 75000.00, "engine": "6-cylinder", "transmission": "Automatic"},
+      {"name":"A4", "type":"SUV", "year": 2023, "car_make":car_make_instances[2], "dealer_id": 3, "price": 60000.00, "engine": "4-cylinder", "transmission": "Automatic"},
+      {"name":"A5", "type":"SUV", "year": 2023, "car_make":car_make_instances[2], "dealer_id": 3, "price": 65000.00, "engine": "4-cylinder", "transmission": "Automatic"},
+      {"name":"A6", "type":"SUV", "year": 2023, "car_make":car_make_instances[2], "dealer_id": 3, "price": 70000.00, "engine": "6-cylinder", "transmission": "Automatic"},
+      {"name":"Sorrento", "type":"SUV", "year": 2023, "car_make":car_make_instances[3], "dealer_id": 4, "price": 45000.00, "engine": "4-cylinder", "transmission": "Automatic"},
+      {"name":"Carnival", "type":"SUV", "year": 2023, "car_make":car_make_instances[3], "dealer_id": 4, "price": 50000.00, "engine": "V6", "transmission": "Automatic"},
+      {"name":"Cerato", "type":"Sedan", "year": 2023, "car_make":car_make_instances[3], "dealer_id": 4, "price": 35000.00, "engine": "4-cylinder", "transmission": "Automatic"},
+      {"name":"Corolla", "type":"Sedan", "year": 2023, "car_make":car_make_instances[4], "dealer_id": 5, "price": 30000.00, "engine": "4-cylinder", "transmission": "CVT"},
+      {"name":"Camry", "type":"Sedan", "year": 2023, "car_make":car_make_instances[4], "dealer_id": 5, "price": 35000.00, "engine": "4-cylinder", "transmission": "Automatic"},
+      {"name":"Kluger", "type":"SUV", "year": 2023, "car_make":car_make_instances[4], "dealer_id": 5, "price": 45000.00, "engine": "V6", "transmission": "Automatic"},
+    ]
+    for data in car_model_data:
+            CarModel.objects.create(
+                name=data['name'], 
+                car_make=data['car_make'], 
+                type=data['type'], 
+                year=data['year'],
+                dealer_id=data['dealer_id'],
+                price=data['price'],
+                engine=data['engine'],
+                transmission=data['transmission']
+            )
